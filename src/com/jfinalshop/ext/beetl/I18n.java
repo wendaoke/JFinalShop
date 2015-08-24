1e66bd2daf3a530dda8c32991d7d227a20c79a71
LiHongYuan <LiHongYuan@192.168.1.120>
2015-08-24 11:57:27 +0800

    Signed-off-by: LiHongYuan 修改国际化问题

--- src/com/jfinalshop/ext/beetl/I18n.java
+++ src/com/jfinalshop/ext/beetl/I18n.java
@@ -7,14 +7,15 @@ import org.beetl.core.Function;
 
 
 
+
 public class I18n implements Function {
 	
 	public Object call(Object[] obj, Context context) {
 		HttpServletRequest req = (HttpServletRequest) context.getGlobal("request");
 		try {
-			return com.jfinal.i18n.I18n.use((String) obj[0], req.getLocale());
+			return com.jfinal.i18n.I18n.use(req.getLocale().toString()).get((String) obj[0]);
 		} catch (NullPointerException e) {
-			return com.jfinal.i18n.I18n.use((String) obj[0]);
+			return com.jfinal.i18n.I18n.use(req.getLocale().toString()).get((String) obj[0]);
 		}
 	}
 
